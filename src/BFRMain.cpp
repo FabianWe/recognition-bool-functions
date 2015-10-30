5375f903444101aa1e3f14594c335cb1883203e4
Fabian Wenzelmann <wenzelmf@informatik.uni-freiburg.de>
2015-10-30 13:43:01 +0100

    added a skeleton for compiling, testing and documentation... not useful now

---
+++ src/BFRMain.cpp
@@ -0,0 +1,26 @@
+/*
+ * BFRMain.cpp
+ *
+ * Copyright 2015 Fabian Wenzelmann
+ *
+ * This file is part of Recognition-Procedures-for-Boolean-Functions.
+ *
+ * Recognition-Procedures-for-Boolean-Functions is free software: you
+ * can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Recognition-Procedures-for-Boolean-Functions is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Recognition-Procedures-for-Boolean-Functions.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+int main(int argc, char** argv) {
+    return 0;
+}
